c67ec7f0374a0094be0ce52ffabb57f062dd0cc4
malachigamblin <malachi.gamblin@gmail.com>
2023-05-27 16:23:19 -0400

    fixed error with add-post.js

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,16 +1,29 @@
-async function logout() {
-  const response = await fetch("/api/users/logout", {
-    method: "post",
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
     headers: {
       "Content-Type": "application/json",
     },
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace("/dashboard");
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector("#logout").addEventListener("click", logout);
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
