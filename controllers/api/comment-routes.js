cd452b0cdea4e69586f0cc6ac9df96979cde59fd
malachigamblin <malachi.gamblin@gmail.com>
2023-05-27 15:12:26 -0400

    setup comment-routes in api

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((e) => {
+      console.log(e);
+      res.status(500).json(e);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((e) => {
+        console.log(e);
+        res.status(400).json(e);
+      });
+  }
+});
+
+module.exports = router;
